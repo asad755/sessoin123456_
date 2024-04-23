6d124229dbcec210ace7dce6db8c183ff61f0726
Asadiftekhar <143116178+asad755@users.noreply.github.com>
2024-04-23 10:32:03 -0700

    ok

---
+++ aliesobj.js
@@ -0,0 +1,12 @@
+//// anonymous
+var teacher = {
+    name: "Zeeshan",
+    exp: 10
+};
+var person = {
+    name: "Zeeshan", //string
+    exp: 10, //number
+    age: 56
+};
+teacher = person;
+console.log(teacher);
---
+++ aliesobj.ts
@@ -0,0 +1,13 @@
+//// anonymous
+let teacher : {name: string, exp: number} = {
+    name: "Zeeshan",
+    exp: 10
+}
+
+let person = {
+    name: "Zeeshan",    //string
+    exp: 10,            //number
+    age: 56
+}
+teacher = person;
+console.log(teacher);
